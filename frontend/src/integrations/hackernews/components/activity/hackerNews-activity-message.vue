0ece3bf11138e5a154430918776fdea5af448b7d
Uroš Marolt <uros@crowd.dev>
2022-11-25 13:12:37 +0100

    bugfix and docker file optimization

---
+++ frontend/src/integrations/hackernews/components/activity/hackerNews-activity-message.vue
@@ -0,0 +1,75 @@
+<template>
+<div v-if="!short && !channelOnly">
+  <a
+    v-if="!short"
+    :href="activity.url"
+    class="text-brand-500 truncate max-w-2xs"
+    target="_blank"
+  >
+    <app-i18n
+      v-if="!channelOnly"
+      :code="computedMessage"
+      :args="computedArgs"
+      :fallback="'entities.activity.fallback'"
+    ></app-i18n>
+  </a>
+  <span v-if="!channelOnly && isComment"
+    >&nbsp;on a
+    <a :href="activity.attributes.parentUrl"> post</a></span
+  >
+  <span v-if="!channelOnly">&nbsp;mentioning&nbsp;</span>
+  {{ activity.channel }}
+  </div>
+  <div v-else-if="short" class="truncate">
+   <app-i18n
+      v-if="!channelOnly"
+      :code="computedMessage"
+      :args="computedArgs"
+      :fallback="'entities.activity.fallback'"
+    ></app-i18n>
+     <span v-if="isComment"
+    >&nbsp;on a post
+    </span
+  >
+  <span v-else
+    >&nbsp;mentioning {{ activity.channel }}</span
+  >
+    
+  </div>
+</template>
+
+<script>
+import AppI18n from '@/shared/i18n/i18n'
+import { computedArgs } from '@/modules/activity/activity.helpers'
+export default {
+  name: 'AppHackerNewsActivityMessage',
+  components: { AppI18n },
+  props: {
+    activity: {
+      type: Object,
+      required: true
+    },
+    short: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    channelOnly: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+  computed: {
+    isComment() {
+      return this.activity.type === 'comment'
+    },
+    computedMessage() {
+      return `entities.activity.${this.activity.platform}.${this.activity.type}`
+    },
+    computedArgs() {
+      return computedArgs(this.activity)
+    }
+  }
+}
+</script>
