f4d34c8811a744f24d1db3ac5c4e270845b64abe
Joana Maia <joana@crowd.dev>
2022-12-20 10:51:38 +0000

    Fix config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -37,9 +37,6 @@ const defaultConfig = {
     process.env.VUE_APP_PIZZLY_PUBLISHABLE_KEY,
   typeformId: process.env.VUE_APP_TYPEFORM_ID,
   typeformTitle: process.env.VUE_APP_TYPEFORM_TITLE,
-  pizzlyUrl: process.env.VUE_APP_PIZZLY_URL,
-  pizzlyPublishableKey:
-    process.env.VUE_APP_PIZZLY_PUBLISHABLE_KEY,
   posthog: {
     apiKey: process.env.VUE_APP_POSTHOG_API_KEY,
     host: process.env.VUE_APP_POSTHOG_HOST
@@ -72,9 +69,6 @@ const composedConfig = {
     'CROWD_VUE_APP_PIZZLY_PUBLISHABLE_KEY',
   typeformId: 'CROWD_VUE_APP_TYPEFORM_ID',
   typeformTitle: 'CROWD_VUE_APP_TYPEFORM_TITLE',
-  pizzlyUrl: 'CROWD_VUE_APP_PIZZLY_URL',
-  pizzlyPublishableKey:
-    'CROWD_VUE_APP_PIZZLY_PUBLISHABLE_KEY',
   posthog: {
     apiKey: 'CROWD_VUE_APP_POSTHOG_API_KEY',
     host: 'CROWD_VUE_APP_POSTHOG_HOST'
